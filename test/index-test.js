313749c7e148b38bf4a73e4b02ce31563a353435
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-27 08:05:36 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,28 +3,28 @@
 // import ReactDOM from 'react-dom'
 // import moment from 'moment'
 // import App from '../src/App'
-// 
+//
 // import Enzyme, { shallow } from 'enzyme';
 // import Adapter from 'enzyme-adapter-react-16';
-// 
+//
 // Enzyme.configure({ adapter: new Adapter() })
-// 
+//
 // describe('App', () => {
 //   let wrapper;
-// 
+//
 //   before(() => {
 //     wrapper = shallow(React.createElement(App));
 //   });
-// 
+//
 //   it('should include "Now" in the header instead of a time', () => {
 //     expect(wrapper.find('header').text()).to.not.include(moment().format('MMMM Do YYYY'))
 //     expect(wrapper.find('header').text()).to.include('Now')
 //   });
-// 
+//
 //   it('should include the ExampleComponent', () => {
 //     expect(wrapper.text()).to.include('<ExampleComponent />')
 //   });
-// 
+//
 //   it('should include the TestComponent', () => {
 //     expect(wrapper.text()).to.include('<TestComponent />')
 //   });
